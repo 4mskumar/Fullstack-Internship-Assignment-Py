56dc14ca8da5b8b566ce40cdc61f6d93ab493b7d
4mskumar <adityakr4ms@gmail.com>
2026-02-23 00:42:30 +0530

    cors update

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "*"],
+    allow_origins=["*", "https://python-fs-next.vercel.app/", "https://python-fs-next.vercel.app/*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
